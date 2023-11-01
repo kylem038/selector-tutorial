0e3d34bae1d26fce537598d5b5ca9702d1167498
kylem038 <misenckj@gmail.com>
2023-11-01 13:58:15 -0600

    Bring in package + setup router

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import store from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  }
+])
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
       
   </React.StrictMode>
